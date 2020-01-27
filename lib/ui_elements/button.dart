79af91880ab6b1d716b64e29c0d29d97213b45ea
Emil Djupvik <emil14833@gmail.com>
2020-01-27 17:35:20 +0100

    add animation

--- lib/ui_elements/button.dart
+++ lib/ui_elements/button.dart
@@ -19,17 +19,25 @@ class Button extends StatefulWidget {
   _ButtonState createState() => _ButtonState();
 }
 
-class _ButtonState extends State<Button> {
+class _ButtonState extends State<Button> with SingleTickerProviderStateMixin {
   bool _tapped = false;
   bool _justTapped = false;
   Duration _currentDuration = Duration();
   Timer _timer;
 
   //TODO: add animation
+  AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 400),
+    );
+    _forwardAnimation();
+
     widget.audio.onDurationChanged.listen((Duration time) async {
       if (_currentDuration != time) {
         _currentDuration = time;
@@ -51,57 +59,69 @@ class _ButtonState extends State<Button> {
     if (_timer != null) _timer.cancel();
   }
 
+  void _forwardAnimation() async {
+    await Future.delayed(Duration(milliseconds: 200));
+    _controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () async {
-        _currentDuration = null;
+    return ScaleTransition(
+      scale: CurvedAnimation(
+        parent: _controller,
+        curve: Curves.elasticOut,
+      ),
+      alignment: Alignment.center,
+      child: GestureDetector(
+        onTap: () async {
+          _currentDuration = null;
 
-        await widget.audio.stop();
-        await widget.audio.play(widget.soundData.url);
+          await widget.audio.stop();
+          await widget.audio.play(widget.soundData.url);
 
-        _justTapped = true;
-        setState(() => _tapped = true);
-        await Future.delayed(Duration(milliseconds: 100));
-        _justTapped = false;
-      },
-      child: Container(
-        margin: EdgeInsets.all(2.0),
-        height: 120,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 3,
-              child: Container(
-                width: 80,
-                alignment: Alignment.center,
+          _justTapped = true;
+          setState(() => _tapped = true);
+          await Future.delayed(Duration(milliseconds: 100));
+          _justTapped = false;
+        },
+        child: Container(
+          margin: EdgeInsets.all(2.0),
+          height: 120,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 3,
                 child: Container(
-                  height: 80,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage("assets/pew-wall.jpg"),
-                      fit: BoxFit.cover,
+                  width: 80,
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: 80,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage("assets/pew-wall.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(4.0),
+                      child: Image.asset(
+                          _tapped ? "assets/face3.png" : "assets/face1.png"),
                     ),
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.all(4.0),
-                    child: Image.asset(
-                        _tapped ? "assets/face3.png" : "assets/face1.png"),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment(0, -0.5),
+                  child: Text(
+                    widget.soundData.name,
+                    style: Theme.of(context).textTheme.subtitle,
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment(0, -0.5),
-                child: Text(
-                  widget.soundData.name,
-                  style: Theme.of(context).textTheme.subtitle,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
